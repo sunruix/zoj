dd5c164ebdf1526675908ec54fcdfd639e52b699
sunrui <icarux@126.com>
2022-03-03 21:06:00 -0500

    fix bugs

--- gb2utf8.sh
+++ gb2utf8.sh
@@ -2,28 +2,28 @@
 
 IFS=$'\n'
 
+# If 2 arguments are given, the second is the new filename
 function conv {
-    file = "$1"
-    newfile = "$1"
+    file="$1"
+    newfile="$1"
     if [ $# > 1 ]; then
-        newfile = "$2"
+        newfile="$2"
     fi
 
-    encoding=$(file --mime-encoding "$file" | grep -o '[^ ]*$')
-    if [ "$encoding" != "utf-8" ]; then
+    encoding=$(file --mime-encoding "$file")
+    if [[ ! "$encoding" =~ "utf-8" ]]; then
         $(iconv "$file" -f gb18030 -t utf-8 -o "$newfile")
     fi
 }
 
-function conv_by_ext {
+# Take one argument as filename's suffix
+function conv_by_suffix {
     for file in $(ls | grep "$1$"); do
-        encoding=$(file --mime-encoding "$file" | grep -o '[^ ]*$')
-        if [ "$encoding" != "utf-8" ]; then
-            f = $(ls "$file" grep '\(.*\.\)*' -o)
-            conv $file "$f.utf8.$1"
-        fi
+	newfile="$(file%.$1).utf8.$1"
+	conv $file $newfile
     done
 
+# Take one argument as filename's suffix
 function conv_recrusive {
     for file in $(ls -1A); do
         if [ -d "$file" ]; then
@@ -32,7 +32,7 @@ function conv_recrusive {
             cd "../"
         fi
     done
-    conv_by_ext "$1"
+    conv_by_suffix "$1"
 }
 
 for file in $@; do
